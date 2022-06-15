0cd29ce3dda899dfa598d11cad4199682e93ff6e
Kuan <kuan@hey.com>
2022-06-15 16:53:29 +0800

    try loading kvs for local dev

--- src/routes/bags/index.svelte
+++ src/routes/bags/index.svelte
@@ -1,5 +1,22 @@
+<script context="module">
+  /** @type {import('./__types/[slug]').Load} */
+  export async function load({ params, fetch, session, stuff }) {
+		const url = "https://4416.pages.dev/api/kvSync.json"
+    const response = await fetch(url);
+
+		console.log('load in bags ', response);
+    return {
+      status: response.status,
+      props: {
+        kvs: response.ok && (await response.json())
+      }
+    };
+  }
+</script>
+
 <script>
   export let bags;
+  export let kvs;
 </script>
 
 <h2>Choose Your Bag</h2>
@@ -8,3 +25,5 @@
 	  <li><a href="bags/{bag.id}">{bag.name}</a></li>
 	{/each}
 </ul>
+
+<p>{kvs}</p>
