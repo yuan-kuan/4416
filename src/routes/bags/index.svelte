e9ff27804d96641876fb180be92e3c92724f6c36
Kuan <kuan@hey.com>
2022-06-19 15:06:50 +0800

    restore bags auto load from endpoint

--- src/routes/bags/index.svelte
+++ src/routes/bags/index.svelte
@@ -1,22 +1,5 @@
-<script context="module">
-  /** @type {import('./__types/[slug]').Load} */
-  export async function load({ params, fetch, session, stuff }) {
-		const url = "https://4416.pages.dev/api/kvSync.json"
-    const response = await fetch(url);
-
-		console.log('load in bags ', response);
-    return {
-      status: response.status,
-      props: {
-        kvs: response.ok && (await response.json())
-      }
-    };
-  }
-</script>
-
 <script>
-  let bags = [{id: "1", name: "one"}];
-  export let kvs;
+  export let bags;
 </script>
 
 <h2>Choose Your Bag</h2>
@@ -25,5 +8,3 @@
 	  <li><a href="bags/{bag.id}">{bag.name}</a></li>
 	{/each}
 </ul>
-
-<p>{JSON.stringify(kvs)}</p>
