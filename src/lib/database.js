fcde2f7326783fb61d5e4d02dc103baaf3f0939d
Kuan <kuan@hey.com>
2022-06-30 14:22:03 +0800

    cache and retrieve response from redis during dev

--- src/lib/database.js
+++ src/lib/database.js
@@ -36,8 +36,39 @@ const fakeBags = [
     { id: 'b_3', name: 'bag three' },
   ];
 
+const fetchBagsRemote = async () => {
+  const response = await fetch("https://api.stripe.com/v1/products", {
+    headers: {
+      Authorization: "Basic c2tfdGVzdF81MUxGQmVZR01DbXJnWEFJdG9DMExYdW1XSWsycUM1a1lKZ1ZrZFF0QnFzSEZ6VGhmaWFvdExPemtDZnNwUk9QaTZOMHZoSXhmeUVUejlSb2ZPUG1rYjRTWTAwRXI0ampaU1I6",
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    method: "GET"
+  });
+
+  return await response.json();
+}
+
 const getAllBags = async () => {
-  return await kv.list('b');
+  const cachethis = import.meta.env.VITE_RECACHE ?? 'no env';
+  console.log('all bag env', cachethis);
+
+  let result;
+  if (dev && cachethis != 1) {
+    result = await kv.get('all_bags'); 
+  } else {
+    result = await fetchBagsRemote();
+    if (cachethis == 1 && dev) {
+      kv.put('all_bags', result);
+    }
+  }
+  const bags = R.pipe(
+    R.prop('data'),
+    // R.pluck('name'),
+  )(result);
+
+  return bags;
+
+  // return await kv.list('b');
 }
 
 const getBag = (/** @type String */ id) =>
