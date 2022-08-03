6c6c428aad9a1be49d6410006c14647a4166bc96
Kuan <kuan@hey.com>
2022-08-03 13:17:17 +0800

    Try building with stripe on Cloudflare pages

--- src/routes/api/checkout.js
+++ src/routes/api/checkout.js
@@ -1,36 +1,36 @@
-// import Stripe from 'stripe';
+import Stripe from 'stripe';
 
-// const YOUR_DOMAIN = 'http://localhost:3000';
+const YOUR_DOMAIN = 'http://localhost:3000';
 
-// /** @type {import('./__types/checkout').RequestHandler} */
-// export const post = async ({ request }) => {
-//   const cart = await request.json();
+/** @type {import('./__types/checkout').RequestHandler} */
+export const post = async ({ request }) => {
+  const cart = await request.json();
 
-//   // Svelte kit cannot build this with adapter, we need to use REST call here.
-//   const stripe = new Stripe('sk_test_51LFBeYGMCmrgXAItoC0LXumWIk2qC5kYJgVkdQtBqsHFzThfiaotLOzkCfspROPi6N0vhIxfyETz9RofOPmkb4SY00Er4jjZSR');
-//   const checkoutSession = await stripe.checkout.sessions.create(
-//     {
-//       line_items: [
-//         {
-//           // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-//           price: cart.bag,
-//           quantity: 1,
-//         },
-//         {
-//           // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-//           price: cart.art,
-//           quantity: 1,
-//         },
-//       ],
-//       mode: 'payment',
-//       success_url: `${YOUR_DOMAIN}/success`,
-//       cancel_url: `${YOUR_DOMAIN}/cancel`,
-//     }
-//   );
+  // Svelte kit cannot build this with adapter, we need to use REST call here.
+  const stripe = new Stripe('sk_test_51LFBeYGMCmrgXAItoC0LXumWIk2qC5kYJgVkdQtBqsHFzThfiaotLOzkCfspROPi6N0vhIxfyETz9RofOPmkb4SY00Er4jjZSR');
+  const checkoutSession = await stripe.checkout.sessions.create(
+    {
+      line_items: [
+        {
+          // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+          price: cart.bag,
+          quantity: 1,
+        },
+        {
+          // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+          price: cart.art,
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: `${YOUR_DOMAIN}/success`,
+      cancel_url: `${YOUR_DOMAIN}/cancel`,
+    }
+  );
 
-//   return {
-//     body: {
-//       'location': checkoutSession.url,
-//     }
-//   };
-// }
+  return {
+    body: {
+      'location': checkoutSession.url,
+    }
+  };
+}
